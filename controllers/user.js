d789c8389d863d37d6d57bf8a8b75f8c7c7d40e4
dd86185 <DietrichDarciJ@JohnDeere.com>
2020-11-14 12:45:24 -0600

    added back bone to all route and controller files

---
+++ controllers/user.js
@@ -0,0 +1,12 @@
+require("dotenv").config;
+
+const User = require('../models').User;
+const Profile = require("../models").Profile;
+const jwt = require('jsonwebtoken');
+const bcrypt = require("bcryptjs");
+const constants = require("../constants");
+
+
+module.exports = {
+
+}
