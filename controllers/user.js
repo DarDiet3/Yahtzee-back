a1cbc0c6553bbbcdebaf0f0ac5cbbaf0b7b010f0
dd86185 <DietrichDarciJ@JohnDeere.com>
2020-11-16 13:44:25 -0600

    get user data

--- controllers/user.js
+++ controllers/user.js
@@ -1,11 +1,28 @@
 require("dotenv").config;
 
 const User = require('../models').User;
+const GameData = require("../models").Gamedata;
 const jwt = require('jsonwebtoken');
 const bcrypt = require("bcryptjs");
 const constants = require("../constants");
 
+const getUserData = (req, res) => {
+    User.findOne({
+        where: {
+            id: req.params.id
+        },
+        include:[GameData]
+    })
+    .then(userData => {
+        res.status(constants.SUCCESS).json({
+            "userData": userData
+        })
+    })
+    .catch(err => {
+        res.status(constants.INTERNAL_SERVER_ERROR).send(`ERROR: ${err}`);
+    })
+}
 
 module.exports = {
-
+    getUserData
 }
